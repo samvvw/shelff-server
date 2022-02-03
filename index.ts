bac36dead30b9579308a3165ec134cbca61cc920
Samvw90 <samuel.villegas90@gmail.com>
2022-02-02 18:32:28 -0800

    chore: initial server setup tsconfig and scripts

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
 import express from 'express'
 
 const app = express()
+const variable: any = 'strindsfdfg'
+app.get('/', (req: any, res) => {
+    const { arg } = req.body
+    res.send(arg)
+})
 
 app.listen(8080, () => console.log('Server running on http://localhost:8080'))
