3a84342f0289c09c46644c4acb401d29c44ce0dc
Samvw90 <samuel.villegas90@gmail.com>
2022-02-02 19:46:06 -0800

    chore: add eslint config and improve dev script

--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
 import express from 'express'
 
 const app = express()
-const variable: any = 'strindsfdfg'
-app.get('/', (req: any, res) => {
-    const { arg } = req.body
-    res.send(arg)
+
+app.get('/', (req, res) => {
+  res.send('Hello Shelff server')
 })
 
 app.listen(8080, () => console.log('Server running on http://localhost:8080'))
