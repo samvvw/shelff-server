297f3b13fb9a6653e9829e2195a4a6838f73a7d3
Sam Villegas <45743681+samvvw@users.noreply.github.com>
2022-02-14 19:47:44 -0800

    feat: #2 create graphql type definitions (#3)

--- index.ts
+++ index.ts
@@ -1,9 +1,42 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import { ApolloServer } from 'apollo-server-express'
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
+import http from 'http'
+import { TypeSource, IResolvers } from '@graphql-tools/utils'
+import { typeDefs, resolvers } from './src/schema'
+import db from './src/config/dbConfig'
 
-const app = express()
+dotenv.config()
+const PORT = process.env.PORT
 
-app.get('/', (req, res) => {
-  res.send('Hello Shelff server')
-})
+const startApolloServer = async (
+  typeDefs: TypeSource,
+  resolvers: IResolvers
+) => {
+  const app = express()
 
-app.listen(8080, () => console.log('Server running on http://localhost:8080'))
+  const httpServer = http.createServer(app)
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  })
+
+  const { rows } = await db.query(`SELECT * from public.user`, [])
+  console.log(rows)
+  await server.start()
+
+  server.applyMiddleware({ app })
+
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: PORT }, resolve)
+  )
+
+  console.log(
+    `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+  )
+}
+
+startApolloServer(typeDefs, resolvers)
---
+++ src/services/user.ts
@@ -0,0 +1,7 @@
+import { DataSource } from 'apollo-datasource'
+
+class UserService extends DataSource {
+  constructor() {
+    super()
+  }
+}
