5df53aa2d16519678486121314a975c4f5e4a4e4
samvvw <warrensamuel90@gmail.com>
2022-02-15 08:17:25 -0800

    chore: remove .env

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -1,16 +1,13 @@
 import { Pool } from 'pg'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const client = new Pool({
-  //   user: 'postgres',
-  //   host: 'localhost',
-  //   database: 'shelff',
-  //   password: 'sam123456',
-  //   port: 5401,
-  user: 'postgres',
-  host: '35.171.223.156',
-  database: 'shelff_dev',
-  password: 'Sh3l11Db@2022',
-  port: 5432,
+  user: process.env.LOCAL_PG_USER,
+  host: process.env.LOCAL_PG_HOST,
+  database: process.env.LOCAL_PH_DATABASE_DEV,
+  password: process.env.LOCAL_PG_PASSWORD,
+  port: Number(process.env.LOCAL_PG_PORT as string),
 })
 
 export default {
