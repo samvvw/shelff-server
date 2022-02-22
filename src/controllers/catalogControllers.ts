52eb41f45953f0727f59d917641bf4d9d8b7c928
José Arteaga <jh.arteaga09@gmail.com>
2022-02-22 15:48:45 -0800

    feat: add query for shelf and location (#25)

--- src/controllers/catalogControllers.ts
+++ src/controllers/catalogControllers.ts
@@ -1,4 +1,4 @@
-import { Category, CatalogContext } from '../../shelff-types'
+import { Category, CatalogContext, Location, Shelf } from '../../shelff-types'
 
 export const getCategories = async (
   parent: undefined,
@@ -7,5 +7,25 @@ export const getCategories = async (
 ) => {
   const response: Category[] | unknown =
     await dataSources.catalogService.getCategories()
+  return response
+}
+
+export const getLocations = async (
+  parent: undefined,
+  args: undefined,
+  { dataSources }: CatalogContext
+) => {
+  const response: Location[] | unknown =
+    await dataSources.catalogService.getLocations()
+  return response
+}
+
+export const getShelves = async (
+  parent: undefined,
+  args: undefined,
+  { dataSources }: CatalogContext
+) => {
+  const response: Shelf[] | unknown =
+    await dataSources.catalogService.getShelves()
   return response
 }
